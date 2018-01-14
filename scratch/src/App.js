3a9bb7f483f8203047c775340442641724212289
kleahey <kevinleahey@C02MV0SGFM74.local>
2018-01-14 08:30:00 -0500

    Added home container and links to navbar

--- scratch/src/App.js
+++ scratch/src/App.js
@@ -1,7 +1,8 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
-import { Navbar } from "react-bootstrap";
+import { Navbar, Nav, NavItem } from "react-bootstrap";
 import "./App.css";
+import Routes from "./Routes";
 
 class App extends Component {
   render() {
@@ -14,7 +15,14 @@ class App extends Component {
             </Navbar.Brand>
             <Navbar.Toggle />
           </Navbar.Header>
+          <Navbar.Collapse>
+            <Nav pullRight>
+              <NavItem href="/signup">Signup</NavItem>
+              <NavItem href="/login">Login</NavItem>
+            </Nav>
+          </Navbar.Collapse>
         </Navbar>
+        <Routes />
       </div>
     );
   }
